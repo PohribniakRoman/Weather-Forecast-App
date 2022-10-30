3854cbb43f86c61357947227848c61344007daff
Roman <roman.pohribniak@gmail.com>
2022-10-30 17:35:56 +0200

    add deploy file

---
+++ prepareDeploy.sh
@@ -0,0 +1,31 @@
+
+echo "clean previous build"
+
+rm -rf dist/
+
+echo "Installing packages"
+
+npm i
+npm run build
+
+echo "Prepearing build..."
+
+rm -rf src/
+
+mv dist/* .
+
+echo "Deploing..."
+
+git branch -D gh-pages 
+
+git checkout -b gh-pages
+
+git add -A
+
+git commit -m "deploy"
+
+git push -f origin gh-pages
+
+echo "Deployed succesfully"
+
+git checkout main
